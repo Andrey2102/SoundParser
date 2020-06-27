662241b3889064c4016bb0a1894b71cea149ea3c
Andrey2102 <46657129+Andrey2102@users.noreply.github.com>
2020-06-27 23:45:36 +0300

    User interface on tkinter was create.

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from __future__ import unicode_literals
 import youtube_dl
 import os
-from sys import argv
+from tkinter import*
+from tkinter import filedialog as fd
+
+
+# Window settings
+
+root = Tk()
+root.geometry("500x170")
+root.title("Sound Parser")
+root.resizable(width=True, height=False)
 
 # Download data and config
+
+def parse():
+    try:
+        os.chdir(PATH.get())
+        try:
+            with youtube_dl.YoutubeDL(download_options) as dl:
+                dl.download([URL.get()])
+        except:
+            STATUS.set("Status: Operation failed. Something wrong with the URL.")
+        STATUS.set("Status: Operation successful.")
+    except:
+        STATUS.set("Status: Operation failed. No such derictory or limited access.")
+
+
+
+
+
 
 download_options = {
     'format': 'bestaudio/best',
@@ -15,13 +41,20 @@ download_options = {
         'preferredquality': '192',
     }],
 }
+lab1 = Label(text="URL address: ").place(x=20, y=10)
+URL = StringVar()
+URL.set("default")
+Entry(width=60, textvariable=URL).place(x=120, y=10)
 
-# Song Directory
-if not os.path.exists('Songs'):
-    os.mkdir('Songs')
-else:
-    os.chdir('Songs')
+lab2 = Label(text="PATH: ").place(x=40, y=40)
+PATH = StringVar()
+PATH.set("C:\\Users\Public\Music")
+Entry(width=60, textvariable=PATH).place(x=120, y=40)
 
-# Download Songs
-with youtube_dl.YoutubeDL(download_options) as dl:
-    dl.download(['https://www.youtube.com/watch?v=lRqyMKoJKaA'])
+STATUS = StringVar()
+STATUS.set("Status: Awaiting the start of work.")
+lab3 = Label(textvariable=STATUS).place(x=0.5, y=150)
+
+but = Button(text="Get Sound", command=parse).place(x=25, y=70)
+
+root.mainloop()
